f369b084a800b522dcbc53b0a62b6b2c063157cc
DariaKorzeniewska <162806@student.uwm.edu.pl>
2021-10-11 17:25:06 +0200

    com

---
+++ zadania1/src/com/company/Main.java
@@ -0,0 +1,122 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //zad1
+        int m1 = 31;
+        int m2 = 28;
+        int m3 = 31;
+        System.out.println(m1+m2+m3);
+
+
+        //zad2
+        int suma=0;
+        for (int i=1; i<10; i++){
+            suma=suma+i;
+            System.out.print(i + "+");
+        }
+        suma+=10;
+        System.out.println("10=" + suma);
+
+
+        //zad3
+        int iloczyn=1;
+        for (int j=1; j<10; j++){
+            iloczyn=iloczyn*j;
+            System.out.print(j + "*");
+        }
+        iloczyn=iloczyn*10;
+        System.out.println("10=" + iloczyn);
+
+
+        //zad4
+        double procent=0.06;
+        double pierwszy, drugi, trzeci;
+        pierwszy = 1000 + 1000*procent ;
+        drugi = pierwszy + pierwszy*procent;
+        trzeci = drugi + drugi*procent;
+        System.out.println("Po pierwszym roku: " + pierwszy );
+        System.out.println("Po drugim roku: " + drugi );
+        System.out.println("Po trzeci roku: " + trzeci );
+
+
+        //zad5
+        System.out.println("+----+");
+        System.out.println("|Java|");
+        System.out.println("+----+");
+        System.out.println("");
+
+        //zad6
+        System.out.println("  ///// ");
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("(| o o |)");
+        System.out.println("  | ^ |  ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+");
+        System.out.println("");
+
+
+        //zad7
+        System.out.println("  *******                                         *      *          ");
+        System.out.println("  *      ***                                      *     *            ");
+        System.out.println("  *         **                                    *    *                   ");
+        System.out.println("  *           *   ****    *****   *   ****        *   *      *****    *****   ********");
+        System.out.println("  *           *       *   *    *  *       *       ****      *     *   *    *         *  ");
+        System.out.println("  *           *   *****   *   *   *   *****       *   *    *       *  *   *        *    ");
+        System.out.println("  *          **  *    *   ****    *  *    *       *    *   *       *  ****       *    ");
+        System.out.println("  *       ***    *   **   *   *   *  *   **       *     *   *     *   *   *    * ");
+        System.out.println("  *******         *** *   *    *  *   *** *       *      *   *****    *    *  ********");
+        System.out.println("");
+        System.out.println("");
+
+        //zad8
+        System.out.println("  /\\_/\\          -----");
+        System.out.println("(  ' '  )      / Hello \\");
+        System.out.println("(   -   )    <   Junior |");
+        System.out.println(" |  |  |       \\ Coder!/");
+        System.out.println("(__ | __)        ----- ");
+        System.out.println("");
+        System.out.println("");
+
+
+        //zad9
+        System.out.println("Harry Potter i Książe Półkrwi");
+        System.out.println("Specter");
+        System.out.println("Spiderman Homecoming");
+        System.out.println("");
+        System.out.println("");
+
+        //zad10
+        System.out.println("Wisława Szymborska - Dłoń");
+        System.out.println("Dwadzieścia siedem kości,");
+        System.out.println("trzydzieści pięć mięśni,");
+        System.out.println("około dwóch tysięcy komórek nerwowych");
+        System.out.println("w każdej opuszce naszych pięciu palców.");
+        System.out.println("To zupełnie wystarczy,");
+        System.out.println("żeby napisać „Mein Kampf”");
+        System.out.println("albo „Chatkę Puchatka”.");
+        System.out.println("");
+        System.out.println("");
+
+        //zad11
+        System.out.println("    *     *     *     *     *    =================================");
+        System.out.println(" *     *     *     *     *       =================================");
+        System.out.println("    *     *     *     *     *  ");
+        System.out.println(" *     *     *     *     *       =================================");
+        System.out.println("    *     *     *     *     *    =================================");
+        System.out.println(" *     *     *     *     *    ");
+        System.out.println("    *     *     *     *     *    =================================");
+        System.out.println(" *     *     *     *     *       =================================");
+        System.out.println("    *     *     *     *     *  ");
+        System.out.println("==================================================================");
+        System.out.println("==================================================================");
+        System.out.println("");
+        System.out.println("==================================================================");
+        System.out.println("==================================================================");
+        System.out.println("");
+        System.out.println("==================================================================");
+        System.out.println("==================================================================");
+    }
+}

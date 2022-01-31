a6a3f031da1860c8dceea51440d2f7a283e649b1
DariaKorzeniewska <162806@student.uwm.edu.pl>
2022-01-31 14:02:39 +0100

    com

---
+++ 162806-E01/Z1/zadanie1_E01/src/com/company/ArreyUtil.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.ArrayList;
+
+public class ArreyUtil {
+    public void main(String[] args) {
+
+    }
+
+    public ArrayList removeRepeatedElements(T obiekt){
+
+
+    }
+}
---
+++ 162806-E01/Z1/zadanie1_E01/src/com/company/T.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.time.LocalDate;
+
+public class T {
+    Character literka;
+    LocalDate data;
+    public void main(String[] args) {
+        this.data = data;
+        this.literka = literka;
+    }
+
+    public void DodajLiterke(Character lit){
+        literka = lit;
+    }
+    public void DodajDate(LocalDate da){
+        data = da;
+    }
+
+}
